cc309f5ce4a613828401bb73fb7247735cf65e0a
Bishal Khadka <47100242+bkhadka2@users.noreply.github.com>
2021-08-19 12:28:58 -0600

    random

--- WriteIn_Application/app.py
+++ WriteIn_Application/app.py
@@ -29,7 +29,8 @@ def index_view():
 
 
 @app.route('/Predict', methods=['POST'])
-def prediction():
+def prediction(): 
+    print('delete this line')
     if request.method == 'POST':
         data = request.get_json()
         imagebase64 = data['image']
